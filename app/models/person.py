beb2ae836aa12e1b64d351a846ef4da8e3b2a9e9
Mistin <mistin_@abv.bg>
2025-05-03 23:11:37 +0300

    pydantic with sqlalchemy models

--- app/models/person.py
+++ app/models/person.py
@@ -1,10 +1,27 @@
 from app.models.user import User 
+from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
 
-class Person(User):
-    def __init__(self, email, password, first_name, name, phone_number, account_type, postal_address,education,skills):
-        super().__init__(email, password, first_name, name, phone_number, account_type, postal_address)
-        self.education = education
-        self.skills = skills
+Base = declarative_base()
 
-    def get_summary(self):
-        return f"{super().get_summary()},{self.name},{self.first_name},{self.phone_number},{self.account_type},{self.postal_address}"
+class Person(Base):
+    __tablename__ = 'person'  # Table name in MySQL
+    
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    first_name = Column(String)
+    name = Column(String)
+    phone_number = Column(String)
+    postal_address = Column(String)
+    account_type = Column(String)
+    education = Column(String)
+    skills = Column(String)
+
+class PersonCreate(User,BaseModel):
+    education:str
+    skills:str
+
+    class Config:
+        orm_mode = True 
--- app/models/user.py
+++ app/models/user.py
@@ -1,15 +1,13 @@
 from abc import ABC
+from pydantic import BaseModel
+class User(ABC,BaseModel):
+    email:str
+    password:str
+    first_name:str
+    name:str
+    phone_number:str
+    account_type:str
+    postal_address:str
 
-class User(ABC):
-    def __init__(self,email,password,first_name,name,phone_number,account_type,postal_address):
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.name = name
-        self.phone_number = phone_number
-        self.account_type = account_type
-        self.postal_address = postal_address
-
-    
     def get_summary(self):
         return f"{self.name},{self.first_name},{self.phone_number},{self.account_type},{self.postal_address}"
