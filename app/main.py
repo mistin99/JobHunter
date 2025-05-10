77bf451be2d34ea389496659fa63c4d0eb4e5f54
Mistin <mistin_@abv.bg>
2025-05-11 01:54:55 +0300

    feat(api) move routes out of main

--- app/main.py
+++ app/main.py
@@ -1,16 +1,10 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from app.models.person import PersonCreate
-from app.crud import create_person
-from app.database import get_db
+from fastapi import FastAPI
+from app.api import auth_routes
 
 app = FastAPI()
+app.include_router(auth_routes.router, prefix="/auth", tags=["auth"])
 
 @app.get("/")
 def read_root():
     return {"message:""Helo"}
 
-@app.post("/register")
-def register(person: PersonCreate, db: Session = Depends(get_db)):
-    person_db = create_person(db=db, person_create=person)  # Call CRUD operation
-    return {"message": "User registered successfully", "email": person_db.email}
