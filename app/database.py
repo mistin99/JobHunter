0664efa93908b35ab69987ff313f71794e3ca5a6
Mistin <mistin_@abv.bg>
2025-05-11 01:53:46 +0300

    chore(db) change engine to work with .env file

--- app/database.py
+++ app/database.py
@@ -2,11 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
-engine = create_engine(
-    settings.database_url
-)
+def get_engine():
+    try:
+        return create_engine(settings.database_url)
+    except Exception as e:
+        print("DB Error:", e)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=get_engine())
 
 def get_db():
     db = SessionLocal()
