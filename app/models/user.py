86e581ffe5bf152fd2eab4d3816c24b65610a1e7
Mistin <mistin_@abv.bg>
2025-05-11 01:58:56 +0300

    feat(user) add validation in base class

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,38 @@
 from abc import ABC
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr,field_validator
+import re
+from app.models.postal_address import PostalAddressCreate
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
 class User(ABC,BaseModel):
-    email:str
+    email:EmailStr
     password:str
     first_name:str
     name:str
     phone_number:str
     account_type:str
-    postal_address:str
+    class Config:
+        from_attributes = True 
+
+    
+    @field_validator("email")
+    def name_must_be_nonempty(cls, v):
+        if not v.strip():
+            raise ValueError("Name cannot be empty")
+        return v
+    
+    @field_validator("password")
+    def password_strength(cls, v):
+        if not all([
+            len(v) >= 8,
+            re.search(r"[A-Z]", v),
+            re.search(r"[a-z]", v),
+            re.search(r"[0-9]", v),
+            re.search(r"[\W_]", v)
+        ]):
+            raise ValueError("Password must be ≥8 chars, with upper, lower, digit, and special char")
+        return v
 
     def get_summary(self):
         return f"{self.name},{self.first_name},{self.phone_number},{self.account_type},{self.postal_address}"
