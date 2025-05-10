3f42415562b6328ed2fc3773caffa2e5e9862ec3
Mistin <mistin_@abv.bg>
2025-05-11 01:57:14 +0300

    re(organization) user pydantic and sqlalchemy model

--- app/models/organization.py
+++ app/models/organization.py
@@ -1,7 +1,27 @@
-from user import User
+from app.models.user import User 
+from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String,ForeignKey
+from sqlalchemy.orm import declarative_base
 
-class Organization(User):
-    def __init__(self, email, password, first_name, name, phone_number, account_type, postal_address,website_url,description):
-        super().__init__(email, password, first_name, name, phone_number, account_type, postal_address)
-        self.website_url = website_url
-        self.description = description
+Base = declarative_base()
+
+class Organization(Base):
+    __tablename__ = "organization"
+    
+    id = Column(Integer,primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    first_name = Column(String)
+    name = Column(String)
+    phone_number = Column(String)
+    postal_address = Column(String)
+    account_type = Column(String)
+    website_url = Column(String)
+    description = Column(String)
+    account_type = Column(String)
+
+class OrganizationCreate(User):
+    website_url:str
+    description:str
+    class Config:
+        from_attributes = True
