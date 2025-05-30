84b7348202bcb2c1eb7ebd60f0d2db19e064c623
ste4o26 <ste4o99@gmail.com>
2025-05-31 00:47:09 +0300

    re: user dtos

--- app/dtos/user.py
+++ app/dtos/user.py
@@ -1,23 +1,36 @@
-from abc import ABC
 from typing import Annotated
 
-from pydantic import BaseModel, BeforeValidator, EmailStr
+from pydantic import BaseModel, BeforeValidator, ConfigDict, EmailStr
 
 from constants import Status
 import dtos.validators as validators
 
 
-class UserDto(ABC, BaseModel):
+class BaseUserDto(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    email: Annotated[EmailStr, BeforeValidator(validators.not_empty)]
+
+
+# TODO add all user fields in here
+class UserDto(BaseUserDto):
     id: int | None = None
     status: Status = Status.PENDING
-    email: Annotated[EmailStr, BeforeValidator(validators.not_empty)]
-    password: Annotated[str, BeforeValidator(validators.strong_password)]
     first_name: Annotated[str, BeforeValidator(validators.not_empty)]
     last_name: Annotated[str, BeforeValidator(validators.not_empty)]
+    password: Annotated[str, BeforeValidator(validators.strong_password)]
     phone_number: Annotated[str, BeforeValidator(validators.not_empty)]
 
-    class Config:
-        from_attributes = True
 
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} - {self.email}."
+class UserSignUpDto(BaseUserDto):
+    id: int | None = None
+    status: Status = Status.PENDING
+    first_name: Annotated[str, BeforeValidator(validators.not_empty)]
+    last_name: Annotated[str, BeforeValidator(validators.not_empty)]
+    password: Annotated[str, BeforeValidator(validators.strong_password)]
+    phone_number: Annotated[str, BeforeValidator(validators.not_empty)]
+
+
+
+class UserSignInDto(BaseUserDto):
+    id: int
+    password: Annotated[str, BeforeValidator(validators.strong_password)]
