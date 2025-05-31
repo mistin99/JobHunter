ca4040894ab6be97e1a1ed1561ca0982ea2f277a
ste4o26 <ste4o99@gmail.com>
2025-06-01 00:59:53 +0300

    chore: Include iorganization routes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
-from api import auth_router
+from api import auth_router, organization_router
 
 app = FastAPI()
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(organization_router, prefix="/organizations", tags=["organizations"])
