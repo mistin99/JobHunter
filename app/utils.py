4c73b13d9fd64fa5f2138a872c46e0b12c3c8a55
ste4o26 <ste4o99@gmail.com>
2025-06-01 00:53:26 +0300

    fix: transactional decorator feat:  extract token id from Authorization header

--- app/utils.py
+++ app/utils.py
@@ -2,8 +2,11 @@ from enum import Enum
 from functools import wraps
 from typing import Any, Callable
 
+from fastapi.security import OAuth2PasswordBearer
 from fastapi import Depends
 from sqlalchemy.orm import Session
+
+from constants import TokenType
 
 
 def convert_enums_to_values(data: dict[str, Any]) -> dict[str, str]:
@@ -19,6 +22,17 @@ def transactional(func: Callable) -> Callable:
     @wraps(func)
     def wrapper(*args, db: Session = Depends(get_db), **kwargs) -> Callable:
         with db.begin():
-            return func(*args, db=db, **kwargs)
+            result = func(*args, db=db, **kwargs)
+            db.flush()
+            db.commit()
+            return result
 
     return wrapper
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/signin")
+
+def get_current_user_id(token: str = Depends(oauth2_scheme)):
+    from services.auth import AuthService
+
+    return AuthService.verify_token(token, TokenType.ACCESS)
