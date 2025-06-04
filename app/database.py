c118be07fc0286c17572b833d0f76a6440eac732
ste4o26 <ste4o99@gmail.com>
2025-06-04 18:14:09 +0300

    chore: increase max allowed packets so we support bigger file uploads

--- app/database.py
+++ app/database.py
@@ -5,7 +5,13 @@ from core.config import settings
 
 
 def _get_session() -> Session:
-    engine_cls = create_engine(settings.database_url, isolation_level="REPEATABLE READ")
+    engine_cls = create_engine(
+        settings.database_url,
+        isolation_level="REPEATABLE READ",
+        pool_pre_ping=True,
+        pool_recycle=3600,
+        connect_args={"max_allowed_packet": 512 * 1024 * 1024},
+    )
     session_facotry = sessionmaker(autocommit=False, autoflush=False, bind=engine_cls)
     return session_facotry()
 
