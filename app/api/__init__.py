783dd1d4603ef09207d6bc98c2dacdee23e2f5e3
ste4o26 <ste4o99@gmail.com>
2025-06-04 18:08:24 +0300

    chore: export user router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,6 @@
 from api.auth import router as auth_router
-from api.organization import router as organization_router
 from api.job_offer import router as job_offer_router
+from api.organization import router as organization_router
+from api.user import router as user_router
 
-__all__ = ["auth_router", "organization_router", "job_offer_router"]
+__all__ = ["auth_router", "organization_router", "job_offer_router", "user_router"]
--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from api import auth_router, job_offer_router, organization_router
+from api import auth_router, job_offer_router, organization_router, user_router
 from core.config import settings
 
 app = FastAPI()
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(organization_router, prefix="/organizations", tags=["organizations"])
 app.include_router(job_offer_router, prefix="/job-offers", tags=["job_offers"])
+app.include_router(user_router, prefix="/users", tags=["users"])
 
 
 app.add_middleware(
