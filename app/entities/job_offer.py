5f97c1d735316dcedf1134e2fcdc8e8eccc53064
ste4o26 <ste4o99@gmail.com>
2025-06-04 18:04:43 +0300

    re: Restructure entities since we decided to use blobs insterad of cloud storage.

--- app/entities/job_offer.py
+++ app/entities/job_offer.py
@@ -1,16 +1,16 @@
 from typing import TYPE_CHECKING
-from sqlalchemy import Column, Enum, ForeignKey, Integer, String, Table, Text
+
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from entities.base import BaseEntity
 from entities.organization import Organization
-from entities.user import User, users_applications
+from entities.user import UserApplication
 
 if TYPE_CHECKING:
     from entities.job_tag import JobTag
 
 
-# M:M table
 job_offers_tags = Table(
     "job_offers_tags",
     BaseEntity.metadata,
@@ -22,20 +22,20 @@ job_offers_tags = Table(
 class JobOffer(BaseEntity):
     __tablename__ = "job_offers"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, nullable=False)
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, index=True, nullable=False
+    )
     title: Mapped[str] = mapped_column(String(50), nullable=False)
     description: Mapped[str] = mapped_column(String(255), nullable=False)
     organization_id: Mapped[int] = mapped_column(ForeignKey("organizations.id"))
 
     # Navigation properties
     tags: Mapped[list["JobTag"]] = relationship(
-        "JobTag",
-        secondary=job_offers_tags,
-        back_populates="job_offers"
+        "JobTag", secondary=job_offers_tags, back_populates="job_offers"
     )
-    organization: Mapped[Organization] = relationship("Organization", back_populates="job_offers")
-    applicants: Mapped[list[User]] = relationship(
-        "User",
-        secondary=users_applications,
-        back_populates="applications",
+    organization: Mapped[Organization] = relationship(
+        "Organization", back_populates="job_offers"
+    )
+    applications: Mapped[list["UserApplication"]] = relationship(
+        "UserApplication", back_populates="job_offer"
     )
